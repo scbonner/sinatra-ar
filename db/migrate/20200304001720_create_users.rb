fd0ee482aecdec61784327a75eec74bae26a44d6
scbonner <codingbootcamp918@gmail.com>
2020-03-09 02:30:27 +0000

    inserting info into ActiveRecord

--- db/migrate/20200304001720_create_users.rb
+++ db/migrate/20200304001720_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
+  def change      
+    # ruby code to modify database goes here
     create_table :users do |t|
       t.string :email
       t.string :name
-      t.string
       # t.type :column_name
     end
   end
 end
+
 
 
--- models/user.rb
+++ models/user.rb
@@ -3,9 +3,20 @@ class User < ActiveRecord::Base
     user.email = "bob@email.com"
     user.name = "Bob"
     user.save
-end 
-  
+end
 
+
+# class User < ActiveRecord:: Base
+# end
+# Create a directory called models 
+# inside the sinatra_ar directory.  
+# Then create a file user.rb in the 
+# models directory with the two lines above.
+
+user = User.new
+  user.email = "bob@email.com"
+  user.name = "Bob"
+  user.save 
   # this will generate the insert sql string
   # INSERT INTO "users"("name","email") VALUES("Bob", "bob@email.com")
   # and sends it to the database
