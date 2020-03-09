dc8df94543b62040261d6802903600eea969608c
scbonner <codingbootcamp918@gmail.com>
2020-03-09 03:07:33 +0000

    rereading homework video, do not have any sound

--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
-  user = User.new
-    user.email = "bob@email.com"
-    user.name = "Bob"
-    user.save
 end
+
 
 
 # class User < ActiveRecord:: Base
@@ -13,10 +10,5 @@ end
 # Then create a file user.rb in the 
 # models directory with the two lines above.
 
-user = User.new
-  user.email = "bob@email.com"
-  user.name = "Bob"
-  user.save 
-  # this will generate the insert sql string
-  # INSERT INTO "users"("name","email") VALUES("Bob", "bob@email.com")
-  # and sends it to the database
+
+
